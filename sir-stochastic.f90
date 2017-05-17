03a6a779daf71da10f8aefd3960c7ecacaa6fd87
wk-tan <wk.tan@outlook.com>
2017-05-17 11:34:31 +0800

    1.1
    1. include the condition `total rate cannot be zero` to avoid division
    of zero
    2. include integer t_int for integer day outputs

--- sir-stochastic.f90
+++ sir-stochastic.f90
@@ -8,11 +8,13 @@ program sir_stochastic
   real :: t, dt
   real :: ranval
   integer :: pops, popi, popr
-  integer :: i
-  character(len=30) :: output
+  integer :: i, t_int
+  character(len=30) :: output, output_int
 
   do i = 1, 100
+     write(*,*) i
      t = 0.0
+     t_int = 0
      pops = 190
      popi = 10
      popr = 0
@@ -20,14 +22,32 @@ program sir_stochastic
      call random_seed
 
      write(output,9) i
+     write(output_int,19) i
      open(6, file = output)
+     open(16, file = output_int)
      write(6,*) t, pops, popi, popr
+     write(16,*) t_int, pops, popi, popr
+     t_int = t_int + 1
+
      do while (t .lt. 100.0)
         rate_total = beta*pops*popi + gamma*popi
+
+        if (rate_total .gt. 0.0) then
+           call random_number(ranval)
+           dt = -1.0*log(ranval)/rate_total
+        else
+           dt = 1000000.0
+        end if
+
+        do while (t_int .lt. t + dt)
+           if (t_int .gt. 100.0) exit
+           write(16,*) t_int, pops, popi, popr
+           t_int = t_int + 1
+        end do
         
-        call random_number(ranval)
-        dt = -1.0*log(ranval)/rate_total
         t = t + dt
+        if (t .gt. 100.0) exit
+        
         p = beta*pops*popi/rate_total
         if (ranval .lt. p) then
            pops = pops - 1
@@ -37,18 +57,14 @@ program sir_stochastic
            popr = popr + 1
         end if
 
-        if (pops .le. 0) pops = 0
-        if (popi .le. 0) then
-           popi = 0
-           write(6,*) t, pops, popi, popr
-           exit
-        end if
         write(6,*) t, pops, popi, popr
      end do
      close(6)
+     close(16)
   end do
 
   write(*,*) ' #Simulation completed'
 9 format('output_',i0,'.txt')
+19format('output_tint_',i0,'.txt')
 end program sir_stochastic
 !///////////////////////////////////////////////////////////////////////////////
